cbccff951010712264ed53d4716036429196e200
root <root@master1.nminternal.com>
2019-12-28 07:31:05 +0530

    Cleanup Dockerfile and others

--- dockerfiles/airflow/files/scripts/uid_entrypoint.sh
+++ dockerfiles/airflow/files/scripts/uid_entrypoint.sh
@@ -1,25 +1,4 @@
 #!/usr/bin/env bash
-set -x 
-
-function set_user {
-export USER_ID=$(id -u)
-export GROUP_ID=$(id -g)
-
-if ! grep -Fq "${USER_ID}" /etc/passwd; then
-   # current user is an arbitrary
-   # user (its uid is not in the
-   # container /etc/passwd). Let's fix that
-   cat /opt/app-root/src/passwd.template | \
-   sed "s/\${USER_ID}/${USER_ID}/g" | \
-   sed "s/\${GROUP_ID}/${GROUP_ID}/g" | \
-   sed "s/\${HOME}/\/opt\/app-root\/src/g" > /etc/passwd
-
-   cat /opt/app-root/src/group.template | \
-   sed "s/\${USER_ID}/${USER_ID}/g" | \
-   sed "s/\${GROUP_ID}/${GROUP_ID}/g" | \
-   sed "s/\${HOME}/\/opt\/app-root\/src/g" > /etc/group
-fi
-}
 
 if [[ ! ${AIRFLOW__SCHEDULER__MAX_THREADS+x} ]]; then
     # Set default for max threads to the number of cores available on the system.
@@ -34,7 +13,6 @@ fi
 
 case "$1" in
   local)
-#    set_user
     echo "${USER_NAME:-default}:x:$(id -u):0:${USER_NAME:-default} user:${HOME}:/sbin/nologin" >> /etc/passwd
     airflow initdb
     airflow scheduler &
@@ -43,19 +21,16 @@ case "$1" in
   scheduler)
     # Run initdb on scheduler startup as scheduler is the only service guaranteed to have a
     # single pod
-#    set_user
     echo "${USER_NAME:-default}:x:$(id -u):0:${USER_NAME:-default} user:${HOME}:/sbin/nologin" >> /etc/passwd
     airflow initdb
     exec airflow "scheduler"
     ;;
   worker)
-#    set_user
     echo "${USER_NAME:-default}:x:$(id -u):0:${USER_NAME:-default} user:${HOME}:/sbin/nologin" >> /etc/passwd
     export C_FORCE_ROOT=true # Celery thinks we're running as root and blocks startup without this
     exec airflow "worker"
     ;;
   flower|version|webserver)
-#    set_user
     echo "${USER_NAME:-default}:x:$(id -u):0:${USER_NAME:-default} user:${HOME}:/sbin/nologin" >> /etc/passwd
     exec airflow "$@"
     ;;

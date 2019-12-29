4a78404404ad862bb6b6ac114580400f075ff11e
root <root@master1.nminternal.com>
2019-12-29 10:38:29 +0530

    Modified example_python_operator Dag

--- dockerfiles/airflow/files/example_dags/example_python_operator.py
+++ dockerfiles/airflow/files/example_dags/example_python_operator.py
@@ -39,10 +39,9 @@ dag = DAG(
 
 
 # [START howto_operator_python]
-def print_context(ds, **kwargs):
+def print_context(**kwargs):
     """Print the Airflow context and ds variable from the context."""
     pprint(kwargs)
-    print(ds)
     return 'Whatever you return gets printed in the logs'
 
 
